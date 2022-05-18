8826b3d56ebf7184c99a50d24e91e3efa96d5583
STUTI <stutipatpatia@gmail.com>
2022-05-18 16:57:32 +0530

    First Commit

---
+++ src/main/java/learningMaven/subproject/App.java
@@ -0,0 +1,7 @@
+package learningMaven.subproject;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/learningMaven/subproject/SeleniumTest.java
@@ -0,0 +1,18 @@
+package learningMaven.subproject;
+
+import org.junit.jupiter.api.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation()
+	{
+		System.err.println("Hello");
+	}
+	@Test
+	public void elementUI()
+	{
+		System.err.println("elementUI");
+	}
+}
+
